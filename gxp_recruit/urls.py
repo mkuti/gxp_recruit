1bdf8ff504e98aa5992975be4aa146c7877a00e0
Maripia Kuti <pionste@gmail.com>
2021-04-13 07:13:41 +0100

    Removed jobs view and url

--- gxp_recruit/urls.py
+++ gxp_recruit/urls.py
@@ -7,7 +7,6 @@ from pages.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('jobs/', include('jobs.urls')),
 ]
 
 # only add this in when in debug mode
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from jobs.models import Job
-from jobs.forms import SearchForm
+'''from jobs.filters import JobFilter'''
 
 def home(request):
-    '''A view that displays the home page with search bar'''
-    print(Job.objects.all())
-    form = SearchForm()
-    q = ''
-    return render(request, 'home.html', {'form': form})
+    '''A view that displays the home page with a search bar'''
+    jobs = Job.objects.all()
+    for job in jobs:
+        print(job.description)
+    
+    return render(request, 'home.html', {'jobs': jobs})
