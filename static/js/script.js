eca9cb6a7831aee12131c219d365491ff06dae55
Maripia Kuti <pionste@gmail.com>
2021-05-04 19:40:50 +0100

    Added click function for jobs nav item

--- static/js/script.js
+++ static/js/script.js
@@ -7,40 +7,9 @@ function myFunction() {
     }
   }
 
-const jobs = document.getElementById("jobs")
-
-document.getElementById("goToJobs").onclick = function () {
-  ScrollingTo(document.documentElement, jobs, 1250);   
-}
-
-  function ScrollingTo(element, to = 0, duration= 1000, scrollToDone = null) {
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
-
-    const animateScroll = (() => {
-
-      currentTime += increment;
-
-      const val = Math.easeInOutQuad(currentTime, start, change, duration);
-
-      element.scrollTop = val;
-
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-	} else {
-		if (scrollToDone) scrollToDone();
-	}
-    });
-
-    animateScroll();
-  };
-
-  Math.easeInOutQuad = function (t, b, c, d) {
-
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
-  };
+$("#goToJobs").click(function(event) {
+  event.preventDefault();
+  $('html, body').animate({
+      scrollTop: $("#jobs").offset().top
+  }, 2000);
+});
