6b816c82f3c67da7418a948b4d6e749d1b478835
Maripia Kuti <pionste@gmail.com>
2021-05-17 15:00:55 +0100

    Reinstalled psycopg2-binary

--- gxp_recruit/settings.py
+++ gxp_recruit/settings.py
@@ -14,27 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.environ.get("DEVELOPMENT"):
-    development = True
-else:
-    development = False
-
-if 'DEVELOPMENT' in os.environ:
-    DEBUG = True
-    STATIC_URL = '/static/'
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info@corklagos.com'
-else:
-    DEBUG = False
-    STATIC_URL = '/staticfiles/'
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,14 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if "SECRET_KEY" in os.environ:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-else:
-    print("SECRET_KEY not found.")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
@@ -103,7 +80,6 @@ WSGI_APPLICATION = 'gxp_recruit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
